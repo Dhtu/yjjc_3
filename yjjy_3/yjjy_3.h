5a64957550dbf05e79666512c4c5856deeb4cc3d
Htu_D <3170102733@zju.edu.cn>
2018-07-18 20:08:04 +0800

    copy

--- yjjy_3/yjjy_3.h
+++ yjjy_3/yjjy_3.h
@@ -1,15 +1,15 @@
 typedef unsigned char	byte;
+#define BUFFERSIZE 4096
+#define OFFSIZE 0x0E
 
 /*文件头结构*/
 typedef struct tagBITMAPFILEHEADER
 {
-	int bfType; /* 通常是 'BM' 。现在来看似乎判断OS/2的标识已无什么意义*/
-	long   bfSize; /* 文件大小，以字节为单位*/
-	int bfReserved1; /*保留，必须设置为0*/
-	int bfReserved2; /*保留，必须设置为0*/
-	long bfOffBits; /*从文件头开始到实际的图象数据之间的字节的偏移量。
-					这个参数是非常有用的，因为位图信息头和调色板的长度会根据不同情况而变化，
-					可以用这个偏移值迅速的从文件中读取到位数据。 */
+	short bfType; /* 通常是 'BM' 。现在来看似乎判断OS/2的标识已无什么意义*/
+	long  bfSize; /* 文件大小，以字节为单位*/
+	short bfReserved1; /*保留，必须设置为0*/
+	short bfReserved2; /*保留，必须设置为0*/
+	long  bfOffBits; /*从文件头开始到实际的图象数据之间的字节的偏移量。*/
 } BITMAPFILEHEADER;
 
 /*信息头结构*/
@@ -18,8 +18,8 @@ typedef struct tagBITMAPINFOHEADER
 	long biSize; /* 信息头大小 */
 	long biWidth; /* 图像宽度 */
 	long biHeight; /* 图像高度 */
-	int biPlanes; /* 必须为1 */
-	int biBitCount; /* 每像素位数，必须是1, 4, 8或24 */
+	short biPlanes; /* 必须为1 */
+	short biBitCount; /* 每像素位数，必须是1, 4, 8或24 */
 	long biCompression; /* 压缩方法 */
 	long biSizeImage; /* 实际图像大小，必须是4的倍数 */
 	long biXPelsPerMeter; /* 水平方向每米像素数 */
